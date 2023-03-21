49e68aa066ed40c0d56d4b9c57574cbbc00a618a
Abhishek Kumar <chocolate.ladu@gmail.com>
2023-03-21 23:01:33 +0530

    Query parameter

--- basicApi.py
+++ basicApi.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI, Path
 
 app = FastAPI();
-person = {
-    1: {
+persons = {
+    0: {
         "name": "Abhishek Kumar",
         "age": 27,
         "role": "UI Developer"
+    },
+    1: {
+        "name": "XYZ",
+        "age": 34,
+        "role": "TSM"
+    },
+    2: {
+        "name": "ZSR",
+        "age": 24,
+        "role": "Software Developer"
     }
 }
 @app.get("/")
@@ -14,4 +24,11 @@ def get_Name():
 
 @app.get("/get-person/{person_id}")
 def get_person(person_id: int = Path(description="ID of the person you want to view", gt=0, lt=5)):
-    return person[person_id]
+    return persons[person_id]
+
+@app.get("/get-person-by-name")
+def get_person_by_name(name : str):
+    for person_id in persons:
+        if persons[person_id]["name"] == name:
+            return persons[person_id]
+    return {"msg": "Data not found"}
