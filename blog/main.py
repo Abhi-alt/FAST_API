9eb19cfd71fd93e4bbcacb474c7d8d6d2de718c9
Abhishek Kumar <chocolate.ladu@gmail.com>
2023-04-03 22:08:06 +0530

    Proper response code

--- blog/main.py
+++ blog/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, HTTPException, Response
 from . import  schemas, model
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from sqlalchemy import select
 
 app = FastAPI()
 
@@ -14,9 +15,24 @@ def get_db():
     finally:
         db.close()
 
-@app.post("/create-user")
+@app.post("/create-user", status_code=status.HTTP_201_CREATED)
 def create_user(user: schemas.User, db: Session = Depends(get_db)):
     user = model.User(name=user.name, city=user.city)
     db.add(user)
     db.commit()
     db.refresh(user)
+
+@app.get("/all-persons", status_code=status.HTTP_200_OK)
+def get_all_persons(db: Session=Depends(get_db)):
+    persons = select(model.User)
+    return db.scalars(persons).all()
+
+@app.get("/user-by-id/{id}", status_code=status.HTTP_200_OK)
+def get_user(id, response: Response ,db: Session = Depends(get_db)):
+    user = select(model.User).where(model.User.id == id)
+    resp = db.scalars(user).first()
+    if not resp:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"User with id {id} is not found")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"message": f"User with id {id} is not found"}
+    return resp
