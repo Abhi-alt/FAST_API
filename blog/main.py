f62eacda25d79158bf58a5b2ab167229ddb08e96
Abhishek Kumar <chocolate.ladu@gmail.com>
2023-04-08 11:46:41 +0530

    Update method

--- blog/main.py
+++ blog/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends, status, HTTPException, Response
 from . import  schemas, model
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
-from sqlalchemy import select
+from sqlalchemy import select, update
 
 app = FastAPI()
 
@@ -22,6 +22,15 @@ def create_user(user: schemas.User, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(user)
 
+@app.delete("/delete-user/{id}", status_code=status.HTTP_200_OK)
+def delete_user(id: int, db: Session=Depends(get_db)):
+    user = db.get(model.User, id)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"User with id {id} is not found in the database")
+    db.delete(user)
+    db.commit()
+    raise HTTPException(status_code=status.HTTP_204_NO_CONTENT,detail=f"User with id {id} is deleted successfully from database")
+
 @app.get("/all-persons", status_code=status.HTTP_200_OK)
 def get_all_persons(db: Session=Depends(get_db)):
     persons = select(model.User)
@@ -36,3 +45,13 @@ def get_user(id, response: Response ,db: Session = Depends(get_db)):
         # response.status_code = status.HTTP_404_NOT_FOUND
         # return {"message": f"User with id {id} is not found"}
     return resp
+
+@app.put('/update-user/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update_user(id: int, resp: schemas.User, db: Session=Depends(get_db)):
+    user = db.get(model.User, id)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"No such user exists with id {id}")
+    stmt = update(model.User).where(model.User.id == id).values({"city" : resp.city, "name": resp.name}) #can be improved. Try hitting db once only
+    db.execute(stmt)
+    db.commit()
+    return {"message": "User Detail Updated"}
